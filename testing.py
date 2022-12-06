52f111488ef26759cfd8ac0eb5c850be734f9a67
Sean Qu <quxw.work@gmail.com>
2022-12-07 00:27:39 +0800

    testing case

--- testing.py
+++ testing.py
@@ -8,11 +8,6 @@ def _number(s: str):
     return res
 
 
-def _parse(s: str):
-    res = parse(s)
-    return res
-
-
 def _calculate(s: str):
     decimal = calculate(s)
     return str(decimal)
@@ -34,92 +29,103 @@ class Testing(unittest.TestCase):
         self.assertTrue(_number("  +0.234 a "), msg="should be Number")
         self.assertTrue(_number("  +0.234.0 "), msg="should be Number")
 
-        self.assertEqual(_number(" - 123"), [], msg="should Not be Number")
-        self.assertEqual(_number(" - 123"), [], msg="should Not be Number")
-        self.assertEqual(_number(" -a123"), [], msg="should Not be Number")
-        self.assertEqual(_number(" a-123"), [], msg="should Not be Number")
-        self.assertEqual(_number(" .123"), [], msg="should Not be Number")
+        self.assertFalse(_number(" - 123"), msg="should Not be Number")
+        self.assertFalse(_number(" - 123"), msg="should Not be Number")
+        self.assertFalse(_number(" -a123"), msg="should Not be Number")
+        self.assertFalse(_number(" a-123"), msg="should Not be Number")
+        self.assertFalse(_number(" .123"), msg="should Not be Number")
 
     def test_expression(self):
-        self.assertTrue(_parse("  123  "), msg="should be Expression")
-        self.assertTrue(_parse("  -123  "), msg="should be Expression")
-        self.assertTrue(_parse("  123.12  "), msg="should be Expression")
-        self.assertTrue(_parse("  -123.12  "), msg="should be Expression")
-        self.assertTrue(_parse("  (123.12)  "), msg="should be Expression")
-        self.assertTrue(_parse("  (-123.12)  "), msg="should be Expression")
-        self.assertTrue(_parse("  ( -123.12 )  "), msg="should be Expression")
-        self.assertTrue(_parse(" 123.12 - 12.3"), msg="should be Expression")
-        self.assertTrue(_parse(" 123.12 - -12.3"), msg="should be Expression")
-        self.assertTrue(_parse(" 123.12 - +12.3"), msg="should be Expression")
-        self.assertTrue(_parse(" 123.12 - (-12.3)"), msg="should be Expression")
-        self.assertTrue(_parse(" 123.12 - (+12.3)"), msg="should be Expression")
-        self.assertTrue(_parse(" ( 123.12 - (-12.3) )"), msg="should be Expression")
-        self.assertTrue(_parse(" ( 123.12 - (+12.3)) + 1"), msg="should be Expression")
+        self.assertTrue(parse("  123  "), msg="should be Expression")
+        self.assertTrue(parse("  -123  "), msg="should be Expression")
+        self.assertTrue(parse("  123.12  "), msg="should be Expression")
+        self.assertTrue(parse("  -123.12  "), msg="should be Expression")
+        self.assertTrue(parse("  (123.12)  "), msg="should be Expression")
+        self.assertTrue(parse("  (-123.12)  "), msg="should be Expression")
+        self.assertTrue(parse("  ( -123.12 )  "), msg="should be Expression")
+        self.assertTrue(parse(" 123.12 - 12.3"), msg="should be Expression")
+        self.assertTrue(parse(" 123.12 - -12.3"), msg="should be Expression")
+        self.assertTrue(parse(" 123.12 - +12.3"), msg="should be Expression")
+        self.assertTrue(parse(" 123.12 - (-12.3)"), msg="should be Expression")
+        self.assertTrue(parse(" 123.12 - (+12.3)"), msg="should be Expression")
         self.assertTrue(
-            _parse(" ( (( 4.12 /(123.12 * 15))+ 1 ) - (+12.3)) + 1"),
+            parse(" ( 123.12 - (-12.3) )"), msg="should be Expression"
+        )
+        self.assertTrue(
+            parse(" ( 123.12 - (+12.3)) + 1"), msg="should be Expression"
+        )
+        self.assertTrue(
+            parse(" ( (( 4.12 /(123.12 * 15))+ 1 ) - (+12.3)) + 1"),
             msg="should be Expression",
         )
         self.assertTrue(
-            _parse(" 2 * ( 123.12 - (+12.3)) + 1"), msg="should be Expression"
+            parse(" 2 * ( 123.12 - (+12.3)) + 1"), msg="should be Expression"
         )
         self.assertTrue(
-            _parse(" ( (123.12 - +12.3) - 1) / 3"), msg="should be Expression"
+            parse(" ( (123.12 - +12.3) - 1) / 3"), msg="should be Expression"
         )
         self.assertTrue(
-            _parse(
+            parse(
                 " ( (123.12 - -12.3) - 1) / ( 3 + ( ( 1 * 2 ) / ( 3 + ( 5*(6-1) - 12.1) - 1)))"
             ),
             msg="should be Expression",
         )
         self.assertTrue(
-            _parse(" max(1) "),
+            parse(" max(1) "),
             msg="should be Expression",
         )
         self.assertTrue(
-            _parse(" sum(1, 2) "),
+            parse(" sum(1, 2) "),
             msg="should be Expression",
         )
         self.assertTrue(
-            _parse(" max (1, 2, 3, 4, 5) "),
+            parse(" max (1, 2, 3, 4, 5) "),
             msg="should be Expression",
         )
         self.assertTrue(
-            _parse(" max (1, 2, 3, 4, 5 ,) "),
+            parse(" max (1, 2, 3, 4, 5 ,) "),
             msg="should be Expression",
         )
         self.assertTrue(
-            _parse(" 1 + sum (1, 2, 3, 4, 5 ,) - 1"),
+            parse(" 1 + sum (1, 2, 3, 4, 5 ,) - 1"),
             msg="should be Expression",
         )
         self.assertTrue(
-            _parse(" 1 + sum (1, 2 - 3, 4, 5 ,) - 1"),
+            parse(" 1 + sum (1, 2 - 3, 4, 5 ,) - 1"),
             msg="should be Expression",
         )
         self.assertTrue(
-            _parse(" 1 + sum (1, (2 - 3), 4, 5 ,) - 1"),
+            parse(" 1 + sum (1, (2 - 3), 4, 5 ,) - 1"),
             msg="should be Expression",
         )
         self.assertTrue(
-            _parse(" 1 + ( sum (1, max(2, 3), 4, 5 ,)) - 1"),
+            parse(" 1 + ( sum (1, max(2, 3), 4, 5 ,)) - 1"),
             msg="should be Expression",
         )
 
-        self.assertEqual(_parse(" a "), [], msg="should NOT be Expression")
-        self.assertEqual(_parse(" - 123 "), [], msg="should NOT be Expression")
-        self.assertEqual(_parse(" - 123 * * 123 "), [], msg="should NOT be Expression")
-        self.assertEqual(_parse(" 12  3 * * 123 "), [], msg="should NOT be Expression")
-        self.assertEqual(
-            _parse(" ( 12  3 * * 123 "), [], msg="should NOT be Expression"
+        self.assertFalse(parse(" a "), msg="should NOT be Expression")
+        self.assertFalse(parse(" - 123 "), msg="should NOT be Expression")
+        self.assertFalse(
+            parse(" - 123 * * 123 "), msg="should NOT be Expression"
         )
-        self.assertEqual(_parse(" (() 12 *  123 )"), [], msg="should NOT be Expression")
-        self.assertEqual(
-            _parse(" (( 12 + 3 * 123 )"), [], msg="should NOT be Expression"
+        self.assertFalse(
+            parse(" 12  3 * * 123 "), msg="should NOT be Expression"
         )
-        self.assertEqual(
-            _parse(" ( 12 - 3 *  123 ) + (5 * 6))"), [], msg="should NOT be Expression"
+        self.assertFalse(
+            parse(" ( 12  3 * * 123 "), msg="should NOT be Expression"
         )
-        self.assertEqual(_parse(" ss(1,2)"), [], msg="should NOT be Expression")
-        self.assertEqual(_parse(" sum()"), [], msg="should NOT be Expression")
+        self.assertFalse(
+            parse(" (() 12 *  123 )"), msg="should NOT be Expression"
+        )
+        self.assertFalse(
+            parse(" (( 12 + 3 * 123 )"), msg="should NOT be Expression"
+        )
+        self.assertFalse(
+            parse(" ( 12 - 3 *  123 ) + (5 * 6))"),
+            msg="should NOT be Expression",
+        )
+        self.assertFalse(parse(" ss(1,2)"), msg="should NOT be Expression")
+        self.assertFalse(parse(" sum()"), msg="should NOT be Expression")
 
     def test_calculate(self):
         self.assertEqual(
